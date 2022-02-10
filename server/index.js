3766265a9e58a9a257330e201033068f8961b75f
iiivan-lemon <lipton2807@yandex.ru>
2022-02-10 03:12:19 +0300

    index-clicker

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const debug = require('debug');
+
+const log = debug('server');
+
+const SERVER_PORT = 3000;
+
+const server = http.createServer((req, res) => {
+	const {url} = req;
+	log('request', url);
+
+	const fileName = url === '/' ? '/index.html' : url;
+
+
+	fs.readFile(`${__dirname}/../public${fileName}`, (err, file) => {
+		if (err) {
+			log(`error`);
+			res.write('404 not found');
+			res.end();
+			return;
+		}
+
+
+		res.write(file);
+		res.end();
+	});
+});
+
+
+
+server.listen(SERVER_PORT);
